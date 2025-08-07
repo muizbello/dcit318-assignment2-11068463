43f18731c7770e613aa7ae001666a8dc54a373fa
muizbello <bellomuiz78+1@gmail.com>
2025-08-07 12:21:34 +0000

    Inheritance and overriding application completed

---
+++ Inheritance_and_Method_Overriding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Inheritance_and_Method_Overriding
+{
+    internal class Program
+    {
+
+        private class Animal
+        {
+            public virtual void MakeSound()
+            {
+                Console.WriteLine("Some generic sound");
+            }
+        }
+
+        private class Dog : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Bark");
+            }
+        }
+
+        private class Cat : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            animal1.MakeSound();
+
+            Dog dog1 = new Dog();
+            dog1.MakeSound();
+
+            Cat cat1 = new Cat();
+            cat1.MakeSound();
+        }
+    }
+}
