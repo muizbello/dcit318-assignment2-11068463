9a44a9471a7dc4523229b44aa7f9a76ff4593451
muizbello <bellomuiz78+1@gmail.com>
2025-08-07 13:22:50 +0000

    Interfaces task done

---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        interface IMovable
+        {
+            void Move();
+        }
+
+        public class Car : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Car is moving");
+            }
+        }
+
+        public class Bicycle : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Bicycle is moving");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Car1.Move();
+
+            Bicycle Bicycle1 = new Bicycle();
+            Bicycle1.Move();
+        }
+    }
+}
